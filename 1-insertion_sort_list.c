e08bd0e3ec5df2b44b2d06a8b60fa2826a51d9fa
Kelvin <koolkt10@gmail.com>
2024-01-23 14:44:14 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,18 +8,34 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
-	int temp;
+	listint_t *node, *temp;
+
+	if (!list)
+		return;
 
 	node = *list;
 	while (node)
 	{
 		while (node->next && (node->n > node->next->n))
 		{
-			temp = node->prev->n;
-			node->prev->n = node->next->n;
-			node->next->n = temp;
-			node = node->prev;
+			temp = node->next;
+			node->next = temp->next;
+			temp->prev = node->prev;
+
+			if (node->prev)
+				node->prev->next = temp;
+
+			if (temp->next)
+				temp->next->prev = node;
+
+			node->prev = temp;
+			temp->next = node;
+
+			if (temp->prev)
+				node = temp->prev;
+			else
+				*list = temp;
+			print_list(*list);
 		}
 		node = node->next;
 	}
